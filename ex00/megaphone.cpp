9371ea924494c1bce985faa517da83a9a016182b
Arik Hativa <arikhativa@gmail.com>
2022-11-22 15:38:53 +0200

    ex00

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: yoav <yoav@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 11:41:17 by yoav              #+#    #+#             */
-/*   Updated: 2022/11/15 13:15:37 by yoav             ###   ########.fr       */
+/*   Updated: 2022/11/22 15:38:16 by yoav             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,23 @@
 
 #define NO_INPUT_MSG	"* LOUD AND UNBEARABLE FEEDBACK NOISE *"
 #define SUCCESS			0
+#define SPACE_CHAR		' '
+
+static void	print_str_upper(const char *str)
+{
+	std::string s(str);
+
+	for (auto it = s.begin() ; it != s.end(); ++it)
+		std::cout << (char)std::toupper(*it);
+}
 
 int	main(int ac, char **av)
 {
 	if (1 == ac)
-	{
-		std::cout << NO_INPUT_MSG << std::endl;
-		return SUCCESS;
-	}
-	(void)ac;
-	(void)av;
+		std::cout << NO_INPUT_MSG;
+	else
+		for (auto i = 1; av[i]; ++i)
+			print_str_upper(av[i]);
+	std::cout << std::endl;
 	return SUCCESS;
 }
