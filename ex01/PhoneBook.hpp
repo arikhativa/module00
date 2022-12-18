b15835ce16452c2ae04380437c5ef2a216f7cdfa
Yoav Rabby <yrabby@c2r7s3.42madrid.com>
2022-12-18 18:05:08 +0100

    mid ex01

--- ex01/PhoneBook.hpp
+++ ex01/PhoneBook.hpp
@@ -1,12 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
+/*   class.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yrabby <yrabby@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/18 14:01:06 by yrabby            #+#    #+#             */
-/*   Updated: 2022/12/18 14:40:36 by yrabby           ###   ########.fr       */
+/*   Created: 2022/12/18 14:00:59 by yrabby            #+#    #+#             */
+/*   Updated: 2022/12/18 15:11:50 by yrabby           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PHONE_BOOK_HPP
+# define PHONE_BOOK_HPP
+
+class PhoneBook
+{
+	private:
+
+	public:
+		PhoneBook();
+		~PhoneBook();
+};
+
+#endif
