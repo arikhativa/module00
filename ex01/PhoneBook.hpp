9df8f6358a869d54872b94345907ec3fa716e1a9
Arik Hativa <arikhativa@gmail.com>
2023-01-26 18:07:52 +0200

    mid ex01

--- ex01/PhoneBook.hpp
+++ ex01/PhoneBook.hpp
@@ -1,25 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   class.hpp                                          :+:      :+:    :+:   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yrabby <yrabby@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/18 14:00:59 by yrabby            #+#    #+#             */
-/*   Updated: 2022/12/18 15:11:50 by yrabby           ###   ########.fr       */
+/*   Updated: 2023/01/26 18:03:27 by yrabby           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHONE_BOOK_HPP
 # define PHONE_BOOK_HPP
 
+# define LIST_MAX_SIZE	8
+
+# include "Contact.hpp"
+
 class PhoneBook
 {
 	private:
-
+		Contact	list[LIST_MAX_SIZE];
 	public:
 		PhoneBook();
 		~PhoneBook();
-};
+};	
 
 #endif
--- ex01/Reader.hpp
+++ ex01/Reader.hpp
@@ -5,16 +5,24 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: yrabby <yrabby@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/18 14:04:23 by yrabby            #+#    #+#             */
-/*   Updated: 2022/12/18 14:41:10 by yrabby           ###   ########.fr       */
+/*   Created: 2022/12/18 14:00:59 by yrabby            #+#    #+#             */
+/*   Updated: 2023/01/26 18:07:37 by yrabby           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef READER_HPP
 # define READER_HPP
 
+# include "Contact.hpp"
+
 class Reader
 {
-}
+	private:
+
+	public:
+		Reader();
+		~Reader();
+		std::string read_cli();
+};
 
 #endif
