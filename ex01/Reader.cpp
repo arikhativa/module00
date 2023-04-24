07ccf4acf398f13af91c6a61a356e4d612412230
Yoav Rabby <yrabby@c2r7s3.42madrid.com>
2023-04-24 13:30:57 +0200

    rdy

--- ex01/Reader.cpp
+++ ex01/Reader.cpp
@@ -6,7 +6,7 @@
 /*   By: yrabby <yrabby@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/18 14:00:59 by yrabby            #+#    #+#             */
-/*   Updated: 2023/04/22 15:47:29 by yrabby           ###   ########.fr       */
+/*   Updated: 2023/04/24 13:29:48 by yrabby           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 Reader::Reader() : _ADD_STR("ADD"),
 					_SEARCH_STR("SEARCH"),
 					_EXIT_STR("EXIT"),
-					_prompt("Ready to add, search or exit?")
+					_prompt("Ready to ADD, SEARCH or EXIT?")
 {
 }
 
@@ -29,6 +29,11 @@ appStatus Reader::cli(void) const
 
 	std::cout << _prompt << std::endl;
 	std::getline(std::cin, input);
+	if (std::cin.eof())
+	{
+		std::cerr <<  "reached EOF. Exiting the program" << std::endl;
+		return EXIT;
+	}
 	if (_ADD_STR == input)
 		return ADD;
 	else if (_SEARCH_STR == input)
@@ -44,11 +49,21 @@ std::string Reader::arg(std::string arg_name)
 
 	std::cout << arg_name << ":" << std::endl;
 	std::getline(std::cin, input);
+	if (std::cin.eof())
+	{
+		std::cerr <<  "reached EOF. Exiting the program" << std::endl;
+		exit(-1);
+	}
 	while (input.empty() || !std::cin.good())
 	{
-		std::cerr << "invalid input, try again" << std::endl;
+		std::cerr << "invalid (empty) input" << std::endl;
 		std::cin.clear();
 		std::getline(std::cin, input);
+		if (std::cin.eof())
+		{
+			std::cerr <<  "reached EOF. Exiting the program" << std::endl;
+			exit(-1);
+		}
 	}
 	return input;
 }
